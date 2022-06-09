2014faccf690490cf65cfb538982545ca3d92a1b
azzurragraziano <azzurragraziano02@email.com>
2022-06-09 16:05:17 +0200

    improvements

--- mail/script.js
+++ mail/script.js
@@ -5,25 +5,41 @@ stampa un messaggio appropriato sull’esito del controllo.*/
 // lista delle email consentite  
 const allowedEmails = ['marcusgoldman@gmail.com', 'elliotalderson@gmail.com', 'harryquebert@gmail.com',          'alaskayoung@me.com'];
 
-// chiedi email all'utente
-const userEmail = prompt('dimmi la tua email');
+const loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener('click',
+    function() {
+        // leggi email dell'utente
+        const userEmail = document.getElementById('user-email').value;
 
-// controllo se è presente nella lista delle email che possono accedere  
-let emailFound = 'not allowed email';
+        // controllo se è presente nella lista delle email che possono accedere  
+        let emailFound = false;
 
-// scorro l'array
-for (let i = 0; i < allowedEmails.length; i++) {
+        // scorro l'array
+        for (let i = 0; i < allowedEmails.length; i++) {
 
-    // per ogni elemento 
-    const email = allowedEmails[i];
+            // per ogni elemento 
+            const email = allowedEmails[i];
 
-    // controllo se userEmail è presente nell'array
-    if (email === userEmail) {
-        emailFound = 'allowed email';
+            // controllo se userEmail è presente nell'array
+            if (email === userEmail) {
+                emailFound = true;
+            }
+        }
+
+        // stampo l'esito del controllo
+        // se è presente stampo "allowed email"
+        // se non è presente stampo "not allowed email"
+        let userMessage;
+
+        if (emailFound === true) {
+            userMessage = 'allowed email';
+        } else {
+            userMessage = 'not allowed email';
+        }
+
+        document.getElementById('user-message').innerHTML = userMessage;
     }
-}
+)
 
-// stampo in console l'esito del controllo
-// se è presente stampo "allowed email"
-// se non è presente stampo "not allowed email"
-console.log(emailFound);
+
+
